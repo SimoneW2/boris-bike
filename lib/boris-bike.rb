a6867605b90996ae8344332bf421c6fe10081c01
Simone <simonewlkr@hotmail.com>
2019-11-01 14:23:47 +0000

    add changes

--- lib/boris-bike.rb
+++ lib/boris-bike.rb
@@ -1,3 +1,11 @@
-class DockingStation
+require './lib/bike.rb'
 
+class DockingStation
+  def release_bike
+    Bike.new
+  end
+
+  def dock(bike)
+
+  end
 end
--- spec/boris-bike_spec.rb
+++ spec/boris-bike_spec.rb
@@ -5,4 +5,24 @@ describe 'DockingStation' do
     dockingstation = DockingStation.new
     expect(dockingstation).to be_instance_of(DockingStation)
   end
-end
+
+  it 'responds to release bike' do
+    dockingstation = DockingStation.new
+    expect(dockingstation).to respond_to(:release_bike)
+  end
+
+  it 'is the bike working' do
+    bike = Bike.new
+    expect(bike.working?).to eq(true)
+  end
+
+  it 'gets docking station to release bike' do
+    dockingstation = DockingStation.new
+    expect(dockingstation.release_bike).to be_instance_of(Bike)
+  end
+
+  it 'retrieves bike' do
+    dockingstation = DockingStation.new
+    expect(dockingstation).to respond_to(:dock)
+    end
+  end
