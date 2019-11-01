4f6c30610ae17149bb9bbc93f99b56995d279e8f
Simone <simonewlkr@hotmail.com>
2019-11-01 17:02:42 +0000

    changed more

--- lib/boris-bike.rb
+++ lib/boris-bike.rb
@@ -1,15 +1,20 @@
 require './lib/bike.rb'
 
 class DockingStation
+
   def release_bike
-    Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
      @bike = bike
   end
+  #
+  #The method below can be replaced with attr_reader 
+  # def bike
+  #   @bike
+  # end
+ attr_reader :bike
 
-  def bike
-    @bike
-  end
 end
--- spec/boris-bike_spec.rb
+++ spec/boris-bike_spec.rb
@@ -18,7 +18,9 @@ describe DockingStation do
 
   it 'gets docking station to release bike' do
     dockingstation = DockingStation.new
-    expect(dockingstation.release_bike).to be_instance_of(Bike)
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
   end
 
   it 'retrieves bike' do
@@ -35,6 +37,9 @@ describe DockingStation do
    bike = Bike.new
    subject.dock(bike)
   expect(subject.bike).to eq bike
-end
-
   end
+
+  it 'raises an error when there are no bikes available' do
+  expect { subject.release_bike }.to raise_error 'No bikes available'
+end
+end
